298f1689d6d26ca42171cfb1e5a2c212f79d04fc
Pip Austin <pippa.austin@live.co.uk>
2023-05-17 11:38:43 +0100

    Cors changed

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ import {
 import express from "express";
 import session from "express-session";
 import bcrypt from "bcrypt";
-import cors from "cors";
+// import cors from "cors";
 import nodemailer from "nodemailer";
 import { getAllDiscounts, getDiscountCode } from "./lib/discount.js";
 import { getDeliveryOptions } from "./lib/delivery.js";
@@ -67,12 +67,12 @@ const transporter = nodemailer.createTransport({
   secure: true,
 });
 
-var corsOptions = {
-  origin: "https://planted.onrender.com",
-  credentials: true,
-};
+// var corsOptions = {
+//   origin: "https://planted.onrender.com",
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
