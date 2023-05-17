aa0cbb7420dd8381dd3b9c5a2d8bcbaa77e253ce
Pip Austin <pippa.austin@live.co.uk>
2023-05-17 08:53:03 +0100

    Added more env vars

--- connect.js
+++ connect.js
@@ -5,7 +5,7 @@ export const connection = await mysql
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    database: "bsdinh8dsqfyrhcjssyy",
+    database: process.env.DB_NAME,
     port: "3306",
   })
   .then((conn) => {
